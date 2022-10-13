dee4cde7ab9a53d5c85930094e53bf767ac48940
Manuel Escalante <maescalantehe@gmail.com>
2022-10-13 05:36:34 -0400

    feature(ecommerce): Added a floating whatsapp icon in website

---
+++ pleni-modules/pleni_website_floating_whatsapp_icon/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Pleni - Whatsapp Floating Icon in Website",
+    'version': '0.1',
+    'category': 'Uncategorized',
+    'license': 'GPL-3',
+    'author': "Manuel Escalante",
+    'website': "https://pleni.app",
+    'depends': ['website'],
+    'data': [
+        'views/assets_frontend.xml',
+        'views/portal_whatsapp_view.xml',
+        'views/website_inherited.xml',
+    ],
+}

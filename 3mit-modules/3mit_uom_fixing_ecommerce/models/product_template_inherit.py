d30a90806a41f955ed399ee62f5f009c596a7e3c
Oriana Graterol <oggs22@gmail.com>
2022-10-25 18:23:50 -0400

    feature(ecommerce): fix to show discount on pagination

--- 3mit-modules/3mit_uom_fixing_ecommerce/models/product_template_inherit.py
+++ 3mit-modules/3mit_uom_fixing_ecommerce/models/product_template_inherit.py
@@ -5,26 +5,26 @@ from odoo import models, fields, api
 from odoo.exceptions import UserError
 
 
-class ProductTemplateInherit(models.Model):
-    _inherit = 'product.template'
+# class ProductTemplateInherit(models.Model):
+#     _inherit = 'product.template'
 
-    def _get_combination_info(self, combination=False, product_id=False, add_qty=1, pricelist=False, parent_combination=False, only_template=False):
-        res = super(ProductTemplateInherit, self)._get_combination_info(combination, product_id, add_qty, pricelist, parent_combination, only_template)
-        uom_id = self.env['product.template'].browse(res.get('product_template_id')).uom_id
-        # res['uom_id'] = uom_id.name
+#     def _get_combination_info(self, combination=False, product_id=False, add_qty=1, pricelist=False, parent_combination=False, only_template=False):
+#         res = super(ProductTemplateInherit, self)._get_combination_info(combination, product_id, add_qty, pricelist, parent_combination, only_template)
+#         uom_id = self.env['product.template'].browse(res.get('product_template_id')).uom_id
+#         # res['uom_id'] = uom_id.name
         
-        # logger.info('uom_id 123456')
-        # logger.info(uom_id)
-        res['show_discount'] = False
-        # product = self.env['product.product'].search([('product_tmpl_id', '=', self.id)])
-        # if not product or not pricelist: return res
-        # pricelist_items = self.env['product.pricelist.item'].sudo().search([('pricelist_id','=',pricelist.id),'|',('product_tmpl_id','=',self.id),('product_id','=',product.id)], order= 'min_quantity asc')
-        # if not pricelist_items: return res
-        # for item in pricelist_items:
-        #     if 1/factor >= item.min_quantity :
+#         # logger.info('uom_id 123456')
+#         # logger.info(uom_id)
+#         res['show_discount'] = False
+#         # product = self.env['product.product'].search([('product_tmpl_id', '=', self.id)])
+#         # if not product or not pricelist: return res
+#         # pricelist_items = self.env['product.pricelist.item'].sudo().search([('pricelist_id','=',pricelist.id),'|',('product_tmpl_id','=',self.id),('product_id','=',product.id)], order= 'min_quantity asc')
+#         # if not pricelist_items: return res
+#         # for item in pricelist_items:
+#         #     if 1/factor >= item.min_quantity :
 
-        # res['wholesale_price'] = pricelist_items[-1].fixed_price if pricelist_items[-1].fixed_price != res['price'] else None
+#         # res['wholesale_price'] = pricelist_items[-1].fixed_price if pricelist_items[-1].fixed_price != res['price'] else None
 
 
-        return res
+#         return res
 

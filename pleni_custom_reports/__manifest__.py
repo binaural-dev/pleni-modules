beea30eacea349050606ed17decbb640d6eb0c93
Manuel Escalante <maescalantehe@gmail.com>
2022-12-01 11:00:10 -0400

    improvement(custom_reports): Minor changes

--- pleni_custom_reports/__manifest__.py
+++ pleni_custom_reports/__manifest__.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Pleni - Custom Reports",
-    'version': '0.1.5',
+    'version': '0.1.6',
     'category': 'Uncategorized',
     'license': 'GPL-3',
     'author': "Manuel Escalante",
     'website': "https://pleni.app",
-    'depends': [
-        'base',
-        'sale',
-        'account'
-    ],
+    'depends': ['base', 'sale', 'account'],
     'data': [
-        'report/report_invoice.xml',
         'report/report_deliveryslip.xml'
+        'report/report_invoice.xml',
     ]
 }

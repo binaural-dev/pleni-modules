4362b90fab4fef447c5ccdda32144522486589a3
Oriana Graterol <oggs22@gmail.com>
2023-03-18 14:25:10 -0400

    fixed show pricelist on shop

--- pleni-modules/pleni_shop/models/models.py
+++ pleni-modules/pleni_shop/models/models.py
@@ -9,6 +9,15 @@ class ProductTemplateInherit(models.Model):
     _inherit = 'product.template'
 
     def _get_combination_info(self, combination=False, product_id=False, add_qty=1, pricelist=False, parent_combination=False, only_template=False):
+        pricelist = pricelist
+        if not pricelist:
+            pricelist_context = dict(self.env.context)
+            if not pricelist_context.get('pricelist'):
+                website = self.env['website'].get_current_website()
+                pricelist = website.get_current_pricelist()
+            else:
+                pricelist = self.env['product.pricelist'].browse(pricelist_context['pricelist'])
+
         res = super(ProductTemplateInherit, self)._get_combination_info(combination, product_id, add_qty, pricelist, parent_combination, only_template)
         uom_id = self.env['product.template'].browse(res.get('product_template_id')).uom_id
         res['uom_id'] = uom_id.name
